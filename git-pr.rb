ca90c0d3accf36339fc4cc6ca06dbb3fd6291aae
Mathias Rasmussen <mathiasvr@gmail.com>
2017-05-14 21:04:10 +0200

    update git-pr

--- git-pr.rb
+++ git-pr.rb
@@ -1,8 +1,8 @@
 class GitPr < Formula
   desc "Git extension for making GitHub pull requests from the command-line."
   homepage "https://github.com/mathiasvr/git-pr"
-  url "https://github.com/mathiasvr/git-pr/archive/v0.1.tar.gz"
-  sha256 "3ecfb8d4b1c25535013b626b029ca7e2811a8e3bb5257aadcebdf0e5285f776c"
+  url "https://github.com/mathiasvr/git-pr/archive/v0.2.tar.gz"
+  sha256 "a70b89f69c1a335144f14292ee784be7d2f85fa97ea5dcdb3e03aa912f1de765"
   head "https://github.com/mathiasvr/git-pr.git"
 
   def install
